9533f28c90c2cb1c775b61ea53faaac6c36a624b
Seungwon <jshtmddnjs@naver.com>
2020-10-15 12:11:15 +0900

    custom toolbar & include(modulizing) & RelativeLayout

--- app/src/main/java/com/codingwithmitch/notes/NoteActivity.java
+++ app/src/main/java/com/codingwithmitch/notes/NoteActivity.java
@@ -3,6 +3,9 @@ package com.codingwithmitch.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codingwithmitch.notes.models.Note;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -13,6 +16,9 @@ public class NoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
-
+        if(getIntent().hasExtra("selected_note")) {
+            Note note = getIntent().getParcelableExtra("selected_note");
+            Log.d(TAG, "onCreate: " + note);
+        }
     }
 }
--- app/src/main/java/com/codingwithmitch/notes/models/Note.java
+++ app/src/main/java/com/codingwithmitch/notes/models/Note.java
@@ -1,6 +1,9 @@
 package com.codingwithmitch.notes.models;
 
-public class Note {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Note implements Parcelable {
 
     private String title;
     private String content;
@@ -15,6 +18,36 @@ public class Note {
     public Note() {
 
     }
+
+    protected Note(Parcel in) {
+        title = in.readString();
+        content = in.readString();
+        timestamp = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(content);
+        dest.writeString(timestamp);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
 
     public String getTitle() {
         return title;
