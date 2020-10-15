e56960ea75dcf3c94a075c06d88b5cf399c36e9a
Seungwon <jshtmddnjs@naver.com>
2020-10-15 15:44:38 +0900

    2 state variable constant & 1 state variable

--- app/src/main/java/com/codingwithmitch/notes/NoteActivity.java
+++ app/src/main/java/com/codingwithmitch/notes/NoteActivity.java
@@ -8,6 +8,8 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.codingwithmitch.notes.models.Note;
@@ -15,20 +17,25 @@ import com.codingwithmitch.notes.models.Note;
 public class NoteActivity extends AppCompatActivity implements
         View.OnTouchListener,
         GestureDetector.OnGestureListener,
-        GestureDetector.OnDoubleTapListener
-        {
+        GestureDetector.OnDoubleTapListener {
 
     private static final String TAG = "SomeActivity";
+    private static final int EDIT_MODE_ENABLED = 1;
+    private static final int EDIT_MODE_DISABLED = 0;
+
 
     // ui component
     private LinedEditText mLinedEditText;
     private EditText mEditTitle;
     private TextView mViewTitle;
+    private RelativeLayout mCheckContainer, mBackArrowContainer;
+    private ImageButton mCheck, mBackArrow;
 
     // vars
     private boolean mIsNewNote;
     private Note mNoteInitial;
     private GestureDetector mGestureDetector;
+    private int mMode;
 
 
     @Override
@@ -38,18 +45,21 @@ public class NoteActivity extends AppCompatActivity implements
         mLinedEditText = findViewById(R.id.note_text);
         mEditTitle = findViewById(R.id.note_edit_title);
         mViewTitle = findViewById(R.id.note_text_title);
+        mCheckContainer = findViewById(R.id.check_container);
+        mBackArrowContainer = findViewById(R.id.back_arrow_container);
+        mCheck = findViewById(R.id.toolbar_check);
+        mBackArrow = findViewById(R.id.toolbar_back_arrow);
 
         setListener();
 
         if (getIncomingIntent()) {
             // this is a new note, (EDIT MODE)
             setNewNoteProperties();
+            enableEditMode();
         } else {
             // this is not a new note, (VIEW MODE)
             setNoteProperties();
         }
-
-
     }
 
     private void setListener() {
@@ -59,21 +69,44 @@ public class NoteActivity extends AppCompatActivity implements
 
     private boolean getIncomingIntent() {
         if (getIntent().hasExtra("selected_note")) {
-           mNoteInitial = getIntent().getParcelableExtra("selected_note");
+            mNoteInitial = getIntent().getParcelableExtra("selected_note");
 
+            mMode = EDIT_MODE_DISABLED;
             mIsNewNote = false;
             return false;
         }
+        mMode = EDIT_MODE_ENABLED;
         mIsNewNote = true;
         return true;
     }
 
-    private void setNoteProperties(){
+    private void enableEditMode() {
+        mBackArrow.setVisibility(View.GONE);
+        mCheckContainer.setVisibility(View.VISIBLE);
+
+        mViewTitle.setVisibility(View.GONE);
+        mEditTitle.setVisibility(View.VISIBLE);
+
+        mMode = EDIT_MODE_ENABLED;
+    }
+
+    private void disableEditMode() {
+        mBackArrow.setVisibility(View.VISIBLE);
+        mCheckContainer.setVisibility(View.GONE);
+
+        mViewTitle.setVisibility(View.VISIBLE);
+        mEditTitle.setVisibility(View.GONE);
+
+        mMode = EDIT_MODE_DISABLED;
+    }
+
+    private void setNoteProperties() {
         mViewTitle.setText(mNoteInitial.getTitle());
         mEditTitle.setText(mNoteInitial.getTitle());
         mLinedEditText.setText(mNoteInitial.getContent());
     }
-    private void setNewNoteProperties(){
+
+    private void setNewNoteProperties() {
         mViewTitle.setText("Note Title");
         mEditTitle.setText("Note Title");
     }
@@ -83,49 +116,50 @@ public class NoteActivity extends AppCompatActivity implements
         return mGestureDetector.onTouchEvent(motionEvent);
     }
 
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
-                return false;
-            }
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        return false;
+    }
 
-            @Override
-            public boolean onDoubleTap(MotionEvent motionEvent) {
-                Log.d(TAG, "onDoubleTap: double tapped.");
-                return false;
-            }
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        Log.d(TAG, "onDoubleTap: double tapped.");
+        enableEditMode();
+        return false;
+    }
 
-            @Override
-            public boolean onDoubleTapEvent(MotionEvent motionEvent) {
-                return false;
-            }
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        return false;
+    }
 
-            @Override
-            public boolean onDown(MotionEvent motionEvent) {
-                return false;
-            }
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
 
-            @Override
-            public void onShowPress(MotionEvent motionEvent) {
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
 
-            }
+    }
 
-            @Override
-            public boolean onSingleTapUp(MotionEvent motionEvent) {
-                return false;
-            }
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
 
-            @Override
-            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-                return false;
-            }
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
 
-            @Override
-            public void onLongPress(MotionEvent motionEvent) {
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
 
-            }
+    }
 
-            @Override
-            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-                return false;
-            }
-        }
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+}
