1177625c3eddaf0ca35603a6f89cd2528cb798c0
Seungwon <jshtmddnjs@naver.com>
2020-10-17 16:14:28 +0900

    migrate database comment

--- app/src/main/java/com/codingwithmitch/notes/persistence/NoteDatabase.java
+++ app/src/main/java/com/codingwithmitch/notes/persistence/NoteDatabase.java
@@ -8,7 +8,7 @@ import androidx.room.RoomDatabase;
 
 import com.codingwithmitch.notes.models.Note;
 
-@Database(entities = {Note.class}, version = 1)
+@Database(entities = {Note.class}, version = 1) // version changes when the entities changes(migrate database)
 public abstract class NoteDatabase extends RoomDatabase {
 
     public static final String DATABASE_NAME = "notes_db";
